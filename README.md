ace5b107e37d47698a27046b6150ccf638e5e3de
Josh <Josh@C44>
2023-05-16 11:40:40 +1000

    Changed mysql to sqlite. I am dumb and mis-read that in the requirements.

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # orkestra_project
 
 # 1. Setup
-    0. Install mysql
     1. pip install -r requirements.txt
-    2. mysql -u root -p -e "create database petsdb"; 
-    3. python manage.py migrate
+    2. python manage.py migrate
 
 # 2. Running Server
     1. python manage.py runserver
--- pets/pets/settings.py
+++ pets/pets/settings.py
@@ -77,23 +77,13 @@ WSGI_APPLICATION = 'pets.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'petsdb',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
